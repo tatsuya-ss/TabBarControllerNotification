8e1ef8e584a7245240ba9d949bd327d12b4a6cb2
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-12-16 17:24:21 +0900

    プロジェクト作成

---
+++ TabBarControllerNotification/Search/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  TabBarControllerNotification
+//
+//  Created by 坂本龍哉 on 2021/12/16.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TabBarControllerNotification/TabBar/TabBarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarViewController.swift
+//  TabBarControllerNotification
+//
+//  Created by 坂本龍哉 on 2021/12/16.
+//
+
+import UIKit
+
+final class TabBarViewController: UITabBarController {
+    
+    private var controllers: [UIViewController] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    init() {
+        super.init(nibName: String(describing: "TabBarViewController"), bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupTabBar() {
+        let reviewVC = ReviewViewController()
+        reviewVC.tabBarItem = UITabBarItem(title: "レビュー", image: .remove, selectedImage: .actions)
+        let searchVC = SearchViewController()
+        searchVC.tabBarItem = UITabBarItem(title: "検索", image: .checkmark, selectedImage: .none)
+        controllers = [reviewVC, searchVC]
+        UITabBar.appearance().isTranslucent = false
+        UITabBar.appearance().backgroundColor = .systemYellow
+        setViewControllers(controllers, animated: true)
+    }
+    
+}
